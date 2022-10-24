83273af2666dd24b039c6d8cbe8c0aab88f9a1fb
Haafidz Nurul Salim <haafidz123@gmail.com>
2022-10-24 21:03:41 +0700

    add migration

---
+++ database/migrations/2022_10_24_124432_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_product');
+            $table->string('harga_product');
+            $table->string('kode_product')->nullable();;
+            $table->string('created_by_product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
