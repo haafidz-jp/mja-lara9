2d753c05edc0f20d947595b61b1825d70baac7ce
Haafidz Nurul Salim <haafidz123@gmail.com>
2022-10-25 22:41:18 +0700

    revisi migrasi

--- database/migrations/2022_10_24_124432_create_products_table.php
+++ database/migrations/2022_10_24_124432_create_products_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('nama_product');
-            $table->string('harga_product');
+            $table->integer('harga_product');
+            $table->integer('no_product');
             $table->string('kode_product')->nullable();;
             $table->string('created_by_product');
             $table->timestamps();
