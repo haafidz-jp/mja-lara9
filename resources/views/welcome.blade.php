9cc28892b5ce9676027957032f8feedfc71e4a91
Haafidz Nurul Salim <haafidz123@gmail.com>
2022-10-27 20:56:42 +0700

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite(['resources/js/app.js'])
-        <title>Laravel</title>
+        <title>CV. MULIA JAYA AGUNG</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
