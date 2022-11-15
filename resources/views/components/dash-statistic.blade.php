3e4eab480494ebf38e9c47b7a2e24737f631e5df
Haafidz Nurul Salim <haafidz123@gmail.com>
2022-11-16 00:11:15 +0700

    test new component statistic

---
+++ resources/views/components/dash-statistic.blade.php
@@ -0,0 +1,131 @@
+<div>
+    <div class="w-full flex items-center justify-center">
+        <div class="py-4 sm:py-6 md:py-8 bg-white rounded-lg">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-6 xl:px-10 gap-y-8 gap-x-12 2xl:gap-x-28">
+                <div class="w-full">
+                    <p tabindex="0" class="focus:outline-none text-xs md:text-sm font-medium leading-none text-gray-500 uppercase">Sales</p>
+                    <p tabindex="0" class="focus:outline-none text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold leading-3 text-gray-800 mt-3 md:mt-5">89.5%</p>
+                    <div class="flex flex-col md:w-64">
+                        <div class="w-full flex justify-end"> 
+                            <div class="flex items-center">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/stats-svg1.svg" alt="green up"/>
+                                
+                                <p tabindex="0" class="focus:outline-none text-xs leading-none text-green-700">4.3%</p>
+                            </div>
+                        </div>
+                        <div class="mt-2.5">
+                            <div class="w-full h-1 bg-gray-200 rounded-full">
+                                <div class="w-1/2 h-1 bg-blue-500 rounded-full"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <p tabindex="0" class="focus:outline-none mt-1.5 text-xs leading-3 text-gray-500">Yearly target</p>
+                </div>
+                <div class="w-full">
+                    <p tabindex="0" class="focus:outline-none text-xs md:text-sm font-medium leading-none text-gray-500 uppercase">revenue</p>
+                    <p tabindex="0" class="focus:outline-none text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold leading-3 text-gray-800 mt-3 md:mt-5">$75,000</p>
+                    <div class="flex flex-col">
+                        <div class="h-4"></div>
+                        <div class="md:w-64 mt-2.5">
+                            <div class="w-full h-1 bg-gray-200 rounded-full">
+                                <div class="w-40 h-1 bg-lime-500 rounded-full"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <p tabindex="0" class="focus:outline-none mt-1.5 text-xs leading-3 text-gray-500">Yearly target</p>
+                </div>
+                <div class="w-full">
+                    <p tabindex="0" class="focus:outline-none text-xs md:text-sm font-medium leading-none text-gray-500 uppercase">customers</p>
+                    <p tabindex="0" class="focus:outline-none text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold leading-3 text-gray-800 mt-3 md:mt-5">3922</p>
+                    <div class="flex flex-col md:w-64">
+                        <div class="w-full flex justify-end">
+                            <div class="flex items-center">
+                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/stats-svg1.svg" alt="green up"/>
+                                
+                                <p tabindex="0" class="focus:outline-none text-xs leading-none text-green-700">9.1%</p>
+                            </div>
+                        </div>
+                        <div class="mt-2.5">
+                            <div class="w-full h-1 bg-gray-200 rounded-full">
+                                <div class="w-44 h-1 bg-yellow-500 rounded-full"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <p tabindex="0" class="focus:outline-none mt-1.5 text-xs leading-3 text-gray-500">Yearly target</p>
+                </div>
+								<div class="w-full">
+									<p tabindex="0" class="focus:outline-none text-xs md:text-sm font-medium leading-none text-gray-500 uppercase">customers</p>
+									<p tabindex="0" class="focus:outline-none text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold leading-3 text-gray-800 mt-3 md:mt-5">3922</p>
+									<div class="flex flex-col md:w-64">
+											<div class="w-full flex justify-end">
+													<div class="flex items-center">
+															<img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/stats-svg1.svg" alt="green up"/>
+															
+															<p tabindex="0" class="focus:outline-none text-xs leading-none text-green-700">9.1%</p>
+													</div>
+											</div>
+											<div class="mt-2.5">
+													<div class="w-full h-1 bg-gray-200 rounded-full">
+															<div class="w-44 h-1 bg-yellow-500 rounded-full"></div>
+													</div>
+											</div>
+									</div>
+									<p tabindex="0" class="focus:outline-none mt-1.5 text-xs leading-3 text-gray-500">Yearly target</p>
+							</div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+			<section class="text-gray-600 body-font">
+				<div class="container px-5 py-24 mx-auto">
+					{{-- <div class="flex flex-col text-center w-full mb-20">
+						<h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
+						<p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+					</div> --}}
+					<div class="flex flex-wrap -m-4 text-center">
+						<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+							<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+									<path d="M8 17l4 4 4-4m-4-5v9"></path>
+									<path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+								</svg>
+								<h2 class="title-font font-medium text-3xl text-gray-900">2.7K</h2>
+								<p class="leading-relaxed">Downloads</p>
+							</div>
+						</div>
+						<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+							<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+									<path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+									<circle cx="9" cy="7" r="4"></circle>
+									<path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+								</svg>
+								<h2 class="title-font font-medium text-3xl text-gray-900">1.3K</h2>
+								<p class="leading-relaxed">Users</p>
+							</div>
+						</div>
+						<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+							<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+									<path d="M3 18v-6a9 9 0 0118 0v6"></path>
+									<path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
+								</svg>
+								<h2 class="title-font font-medium text-3xl text-gray-900">74</h2>
+								<p class="leading-relaxed">Files</p>
+							</div>
+						</div>
+						<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+							<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+									<path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+								</svg>
+								<h2 class="title-font font-medium text-3xl text-gray-900">46</h2>
+								<p class="leading-relaxed">Places</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+</div>
